cc884ed841f4f79769fd871231783e8ce99f18a3
JoostvanBussum <joost1999@hotmail.com>
2020-09-22 14:59:04 +0200

    Made a start with neural network exercise

---
+++ NeuralNetwork/opdracht.py
@@ -0,0 +1,15 @@
+def class neuron(self):
+    # Neuron initialiser with inputs(which is its input neurons with their weights) and it's threshold
+    __init__(self, inputs, ownWeight, neuronThreshold):
+        input = inputs
+        threshold = neuronTreshold
+
+    def recursiveFunciotn:
+        if len(inputs) <= 1:
+            return 1
+        if x == 0:
+
+
+neuron1 = 
+neuron2 = 
+neuron3 = neuron(list[[neuron1, neuron1.getOwnWeight], [neuron2, neuron2.getOwnWeight]])
