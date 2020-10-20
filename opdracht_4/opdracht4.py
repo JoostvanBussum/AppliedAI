0754a703797bc3ac0bb1c9fe04ef539665e03057
JoostvanBussum <joost1999@hotmail.com>
2020-10-20 14:50:23 +0200

    Finished exercise

--- opdracht_4/opdracht4.py
+++ opdracht_4/opdracht4.py
@@ -1,58 +1,23 @@
 import numpy as np
 import tensorflow as tf
 
-import pickle , gzip , os
-from urllib import request
-from pylab import imshow , show , cm
+tf.random.set_seed(0)
 
-# Virgin reader code
-#========================================================================================
-"""
-url = "http://deeplearning.net/data/mnist/mnist.pkl.gz"
-
-if not os.path.isfile("mnist.pkl.gz"):
-    request.urlretrieve(url, "mnist.pkl.gz")
-
-f = gzip.open('mnist.pkl.gz', 'rb')
-train_set , valid_set , test_set = pickle.load(f, encoding='latin1')
-f.close()
-
-def get_image(number):
-    (X, y) = [img[number] for img in train_set]
-    return (np.array(X), y)
-
-def view_image(number):
-    (X, y) = get_image(number)
-    print("Label: %s" % y)
-    imshow(X.reshape(28,28), cmap=cm.gray)
-    show()
-"""
-
-# Chad tensorflow documented code
-#========================================================================================
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
-
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28)),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dropout(0.2),
-    tf.keras.layers.Dense(10)
-])
-
-predictions = model(x_train[:1]).numpy()
-
-tf.nn.softmax(predictions).numpy()
-
-print(predictions)
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Conv2D(filters=32, kernel_size=5, activation='relu', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Conv2D(filters=64, kernel_size=5, activation='relu'))
+model.add(tf.keras.layers.MaxPool2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
 
 
-"""
-input_layer = tf.reshape(train_set, [-1, 28, 28, 1])
-
-conv1 = tf.keras.layers.Conv2D(inputs = input_layer,filters=32,kernel_size=5,padding="same",activation='relu')
-"""
-#=============================================================================================================
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam', loss=loss_fn, metrics=['accuracy'])
+history = model.fit(x_train.reshape(-1, 28, 28, 1), y_train, epochs=5, validation_data=(x_test.reshape(-1, 28, 28, 1), y_test))
