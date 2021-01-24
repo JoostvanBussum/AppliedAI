15409e4f7256b320643acbc6f75288eada0aa386
JoostvanBussum <joost1999@hotmail.com>
2021-01-25 00:56:58 +0100

    Further development on the exercise

--- Opdracht_5/main.py
+++ Opdracht_5/main.py
@@ -7,25 +7,6 @@ import copy
 import math
 import random
 
-# Crossover Lambda's
-#====================
-
-#crossoverLambda = lambda child1, child2
-
-def crossoverSinglePoint(parents):
-
-    pointIndex = random.randint(1, parents[0].genotypeLength-1)
-
-    child1Genotype = parents[0].genotype[:pointIndex]
-    child1Genotype.append(parents[1].genotype[pointIndex:])
-
-    child2Genotype = parents[1].genotype[:pointIndex]
-    child2Genotype.append(parents[0].genotype[pointIndex:])
-
-    lijstje = [geneticIndividual(genotype = child1Genotype), geneticIndividual(genotype = child2Genotype)]
-    return lijstje
-#====================
-
 class geneticIndividual:
     def __init__(self, genotypeLength =10, genotype =None):
         self.fitness = 0
@@ -71,35 +52,35 @@ class geneticIndividual:
 
         
 class evolutionaryAlgorithm:
-    population = []
+    #population = []
 
-    def __init__(self, populationSize =1000, numberOfGenerations =100, genotypeLength =10, mutateChance =0.01, retainModifier =0.2):
+    def __init__(self, populationSize =1000, numberOfGenerations =5, genotypeLength =10, mutateChance =0.01, retainModifier =0.2):
         self.populationSize = populationSize
+        self.generations = numberOfGenerations
         self.genotypeLength = genotypeLength
         self.mutateChance = mutateChance
         self.retainModifier = retainModifier
         self.retainLenght = abs(int(self.populationSize*retainModifier))
+        self.population = []
 
     # Function that randomly generates a starting population
     def generatePopulation(self):
         for i in range(self.populationSize):
             individual = geneticIndividual()
             self.population.append(individual)
-        return
             
     # Function to print the population
     def printPopulation(self):
         graded = sorted(self.population, key=lambda x: x.fitness)
         for individual in graded:
             individual.printGeneticIndividual()
-        return
     
     # TODO implement evolve functie
-    def evolve(self, crossoverFunction):
+    def evolve(self):
         # Sort the individuals by fitness in a new list
         graded = sorted(self.population, key=lambda x: x.fitness)
         retainedParents = graded[:self.retainLenght] #TODO fitness ff fixen (fitness 2800 is nu 'beste' individual)
-            
+        
         # Keep some less good parents for more diversity
         # Function or class parameter?
         random_chance = 0.5
@@ -107,37 +88,67 @@ class evolutionaryAlgorithm:
             if random_chance >= random.uniform(0, 100):
                 retainedParents.append(individual)
 
+        # Even out the list to a even number if necessary
+        if len(retainedParents) % 2 == 1:
+            retainedParents.append(graded[random.randint(0, self.retainLenght)])
+            
         offspringAmount = self.populationSize - len(retainedParents)
         children = []
-        while  len(children) <= offspringAmount:
-            offspringChildren = crossoverFunction(random.sample(retainedParents, 2))
+        while  len(children) < offspringAmount:
 
-            #children.append(crossover(random.sample(retainedParents, 2)))
+            # Make offspring
+
+            offspringChildren = self.crossover(random.sample(retainedParents, 2))
+
+            # Perform possible mutation on all the children
+            children.append(self.mutate(offspringChildren[0]))
+            children.append(self.mutate(offspringChildren[1]))
+            #print("children list: ", children)
 
 
-        retainedParents.append(children)
-        self.population = newPopulation
-        return
-        
+        retainedParents.extend(children)
+        self.population = retainedParents.copy()
         
     # TODO implement crossover function
-    def crossover(self, crossoverFunction):
-        return
+    def crossover(self, parents):
+        
+        #print("parents to sexs: ", parents)
+        pointIndex = random.randint(1, parents[0].genotypeLength-1)
+
+        child1Genotype = parents[0].genotype[:pointIndex]
+        child1Genotype.extend(parents[1].genotype[pointIndex:])
+
+        child2Genotype = parents[1].genotype[:pointIndex]
+        child2Genotype.extend(parents[0].genotype[pointIndex:])
+
+        print("Child 1 genotypes:", child1Genotype)
+        print("Child 2 genotypes:", child2Genotype)
+        children = [geneticIndividual(genotype = child1Genotype), geneticIndividual(genotype = child2Genotype)]
+        print("Genotypelength: ", len(children[0].genotype))
+        return children
 
     # Function to mutate a population, the mutateChance is in percentage
     # We mutate by swapping one value to the other pile
-    def mutate(self):
+    def mutate(self, individual):
 
         # For each gene for each individual in the population there is a chance equal to mutateChance to be mutated
-        for individual in self.population:
-            for gene in range(len(individual.genotype)):
-                if random.uniform(0, 100) <= self.mutateChance:
-                    individual.genotype[gene] ^= 1
+        for gene in range(len(individual.genotype)):
+            if random.uniform(0, 100) <= self.mutateChance:
+                individual.genotype[gene] != individual.genotype[gene]
 
-        return
+        return individual
+
+    def run(self):
+        generationcounter = 0
+        for iteration in range(self.generations):
+            self.evolve()
+            generationcounter += 1
+
+        return sorted(self.population, key=lambda x: x.fitness)
 
 
 algoritme = evolutionaryAlgorithm()
 algoritme.generatePopulation()
 algoritme.printPopulation()
-algoritme.evolve(crossoverSinglePoint)
+algoritme.run()
+print("Result: ", algoritme.printPopulation())
